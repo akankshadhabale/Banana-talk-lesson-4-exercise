eee5d9532a405a7bd0045e0b46ee91f004ff53be
akanksha dhabale <akankshadhabale9158@gmail.com>
2021-09-05 20:37:01 +0530

    translation done

--- app.js
+++ app.js
@@ -2,8 +2,30 @@ var btnTranslate=document.querySelector("#btn-translate");
 var txtInput=document.querySelector("#txt-input");
 var outputDiv=document.querySelector("#output");
 
-function clickedHandler(){
-    outputDiv.innerText="translated  "+ txtInput.value;
+var serverURL="https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(text){
+    return serverURL + "?" + "text=" + text
+
 }
 
-btnTranslate.addEventListener("click",clickedHandler);
+function errorHandler(error){
+    console.log("error occured", error)
+}
+
+function clickedHandler(){
+
+var inputText = txtInput.value;
+
+
+  fetch(getTranslationURL(inputText))
+  .then(response => response.json())
+  .then(json => {
+      var translatedText=json.contents.translated;
+    outputDiv.innerText=translatedText;
+    })
+.catch(errorHandler) 
+}
+
+btnTranslate.addEventListener("click",clickedHandler)
+
